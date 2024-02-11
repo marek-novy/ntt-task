6d1caf7967ad25989ee575e5b9d1c842878286d3
Kuchto47 <martin.k738@gmail.com>
2024-02-11 16:59:24 +0100

    enhance dogs root component

--- apps/frontend/src/routes/dogs.tsx
+++ apps/frontend/src/routes/dogs.tsx
@@ -1,14 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { PaginableDogsDto } from "../model/types";
+import { CachedDogsData, PaginableDogsDto } from "../model/types";
 import { DogsTable } from "../dogs/components/DogsTable";
 import { Pagination } from "../dogs/components/Pagination";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFetchDogs } from "../dogs/hooks/useFetchDogs";
 
 export const Route = createFileRoute("/dogs")({
   component: Dogs,
 });
 
 function Dogs() {
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState<number>(Number.MAX_SAFE_INTEGER);
+  const [cachedDogData, setCachedDogData] = useState<CachedDogsData>({});
+
+  const fetchDogs = useFetchDogs();
+
   const [data, setData] = useState<PaginableDogsDto>(
     {
       dogs: [
@@ -37,6 +44,10 @@ function Dogs() {
       }
     ))
   }
+
+  useEffect(() => {
+    fetchDogs(1);
+  }, []);
 
   return (
     <div className="p-2">

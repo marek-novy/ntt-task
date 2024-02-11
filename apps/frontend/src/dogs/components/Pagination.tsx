b314b7575d729097895b9f91b85ceb26de864d12
Kuchto47 <martin.k738@gmail.com>
2024-02-11 22:31:25 +0100

    improve pagination navigation

--- apps/frontend/src/dogs/components/Pagination.tsx
+++ apps/frontend/src/dogs/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 import { Route } from "../../routes/dogs";
 import { dogsState } from "../state/DogsState";
 import { getVisibleNumbers } from "../utils/visiblePagesGenerator";
@@ -6,39 +6,44 @@ import { observer } from "mobx-react";
 
 export const Pagination = observer(() => {
     const { page } = Route.useSearch();
+    const navigate = useNavigate({from: Route.fullPath});
     const { totalPages } = dogsState;
     return (
       <div className="flex justify-center items-center mt-4">
           <div className="flex space-x-2">
             {page !== 1 && (
-                <Link to="/dogs" search={{page: 1}}>
-                    <button
-                        className="bg-blue-500 text-white px-3 py-1 rounded focus:outline-none focus:shadow-outline"
-                    >
-                        First
-                    </button>
-                </Link>
+                <button
+                    className="bg-blue-500 text-white px-3 py-1 rounded focus:outline-none focus:shadow-outline"
+                    onClick={() => {
+                        navigate({search: {page: 1}})
+                    }}
+                >
+                    First
+                </button>
             )}
             {getVisibleNumbers(page, totalPages).map((number, index) => {
                 const isCurrent = number === page;
                 return (
-                    <Link to="/dogs" search={{page: number}} key={index}>
-                        <button
-                            className={`${isCurrent ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} px-3 py-1 rounded focus:outline-none focus:shadow-outline`}
-                        >
-                            {number}
-                        </button>
-                    </Link>
+                    <button
+                        key={index}
+                        className={`${isCurrent ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'} px-3 py-1 rounded focus:outline-none focus:shadow-outline`}
+                        onClick={() => {
+                            navigate({search: {page: number}})
+                        }}
+                    >
+                        {number}
+                    </button>
                 );
             })}
             {page !== totalPages && (
-                <Link to="/dogs" search={{page: totalPages}}>
-                    <button
-                        className="bg-blue-500 text-white px-3 py-1 rounded focus:outline-none focus:shadow-outline"
-                    >
-                        Last
-                    </button>
-                </Link>
+                <button
+                    className="bg-blue-500 text-white px-3 py-1 rounded focus:outline-none focus:shadow-outline"
+                    onClick={() => {
+                        navigate({search: {page: totalPages}})
+                    }}
+                >
+                    Last
+                </button>
             )}
           </div>
         </div>

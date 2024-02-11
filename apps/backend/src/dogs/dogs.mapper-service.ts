02de7933305574a506c33a734f9ccbaba1fc4739
Kuchto47 <martin.k738@gmail.com>
2024-02-11 12:14:32 +0100

    refactor dogs mapper

--- apps/backend/src/dogs/dogs.mapper-service.ts
+++ apps/backend/src/dogs/dogs.mapper-service.ts
@@ -3,20 +3,20 @@ import { DogEntity } from './entities/dog.entity';
 
 export const mapRawDogsToEntities = (allDogs: string): DogEntity[] => {
   const splitDogs = allDogs.split('\n');
-  return splitDogs.map(mapRawDogToEntity);
+  return splitDogs.map(mapSingleRawDogToEntity);
 };
 
-export const mapRawDogToEntity = (dog: string): DogEntity => {
+const mapSingleRawDogToEntity = (dog: string): DogEntity => {
   return {
     breedNames: dog.split(', '),
   };
 };
 
 export const mapDogsEntitiesToDtos = (dogs: DogEntity[]): DogDto[] => {
-  return dogs.map(mapDogEntityToDto);
+  return dogs.map(mapSingleDogEntityToDto);
 };
 
-export const mapDogEntityToDto = (dog: DogEntity): DogDto => {
+export const mapSingleDogEntityToDto = (dog: DogEntity): DogDto => {
   const breedName = dog.breedNames[0];
   const restOfNames = dog.breedNames.slice(1);
   const synonyms: string[] | undefined =

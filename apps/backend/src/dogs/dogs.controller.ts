a8b271eb89dc84a2becba8f4160c5f1b45b33ad1
Kuchto47 <martin.k738@gmail.com>
2024-02-11 12:03:16 +0100

    implement dog service

--- apps/backend/src/dogs/dogs.controller.ts
+++ apps/backend/src/dogs/dogs.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { DogDto } from './dtos/dog.dto';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { DogsService } from './dogs.service';
+import { PaginableDogsDto } from './dtos/paginable-dogs.dto';
+import { InvalidPageRequestedError } from './errors/InvalidPageRequestedError';
 
 @Controller('dogs')
 export class DogsController {
   constructor(private readonly dogsService: DogsService) {}
 
   @Get()
-  getDogsPaginated(@Query('page') page: number): DogDto[] {
-    return this.dogsService.getNthPageOfDogs(page);
+  getDogsPaginated(
+    @Query('page', ParseIntPipe) page: number,
+  ): PaginableDogsDto {
+    try {
+      return this.dogsService.getNthPageOfDogs(page);
+    } catch (err) {
+      if (err instanceof InvalidPageRequestedError) {
+        throw new BadRequestException();
+      }
+    }
   }
 }
--- apps/backend/src/dogs/dogs.service.ts
+++ apps/backend/src/dogs/dogs.service.ts
@@ -1,10 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { DogDto } from './dtos/dog.dto';
+import { PaginableDogsDto } from './dtos/paginable-dogs.dto';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+import { DogEntity } from './entities/dog.entity';
+import {
+  mapDogsEntitiesToDtos,
+  mapRawDogsToEntities,
+} from './dogs.mapper-service';
+import { InvalidPageRequestedError } from './errors/InvalidPageRequestedError';
 
 @Injectable()
 export class DogsService {
-  getNthPageOfDogs(page: number): DogDto[] {
-    page;
-    return [];
+  private readonly dogs: DogEntity[];
+  private readonly totalPages: number;
+
+  constructor() {
+    this.dogs = this.getDogs();
+    this.totalPages = Math.ceil(this.dogs.length / 10);
+  }
+
+  getNthPageOfDogs(page: number): PaginableDogsDto {
+    if (page <= 0 || page > this.totalPages) {
+      throw new InvalidPageRequestedError();
+    }
+    const slicingNumber = (page - 1) * 10;
+
+    return {
+      dogs: mapDogsEntitiesToDtos(
+        this.dogs.slice(slicingNumber, slicingNumber + 9),
+      ),
+      page,
+      totalPages: this.totalPages,
+    };
+  }
+
+  private getDogs(): DogEntity[] {
+    const path = resolve(__dirname, '../../src/assets/dog-breeds.txt');
+    try {
+      return mapRawDogsToEntities(readFileSync(path, 'utf-8'));
+    } catch (e) {
+      console.error('Reading of dogs file errored...');
+    }
   }
 }
